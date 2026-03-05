e2ad19c57515c6bcd9e39ea0bf8e57498e89fef6
13inuo0 <13inu.o0@gmail.com>
2026-03-05 15:31:43 +0900

    동기식 코드

--- async.js
+++ async.js
@@ -1,1 +1,17 @@
-function receive A(){}
+function receiveA() {
+  console.log("철수에게 숙제를 전달받았습니다.");
+}
+function receiveB() {
+  // 코드 실행에 5초가 걸린다.
+  const wakeUpTime = Date.now() + 5000;
+  while (Date.now() < wakeUpTime) {}
+
+  console.log("유리에게 숙제를 전달받았습니다.");
+}
+function receiveC() {
+  console.log("맹구에게 숙제를 전달받았습니다.");
+}
+
+receiveA();
+receiveB();
+receiveC();
